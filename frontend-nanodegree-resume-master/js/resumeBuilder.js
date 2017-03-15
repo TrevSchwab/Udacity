b9f8e52364699285b64b9722665a4b6b024e477b
Trevor Schwab <trevschwab@gmail.com>
2017-03-15 13:40:50 -0500

    js

---
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -0,0 +1,9 @@
+/*var awesomeThoughts =
+	"My name is Trevor and I am AWESOME!";
+var funThoughts =
+	awesomeThoughts.replace("AWESOME!", "FUN");
+$("#main").append(funThoughts);*/
+
+var formattedName =
+	HTMLheaderName.replace("%data%", "Trevor Schwab");
+$("#main").prepend(formattedName);
